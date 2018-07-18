a652d22133cdf31006c3460d39710733a673a5ed
bergmannlucas <lucaasberg@gmail.com>
2018-07-17 22:01:32 -0300

    Pedido, Pagamento e EstadoPagamento

---
+++ src/main/java/com/bergmannlucas/cursomc/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.bergmannlucas.cursomc.cursomc.repositories;
+
+import com.bergmannlucas.cursomc.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+
+}
