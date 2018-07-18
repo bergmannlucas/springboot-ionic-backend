cadcbee74aaf44c7a8adb71d04fbb8968b43f8fe
bergmannlucas <lucaasberg@gmail.com>
2018-07-17 23:13:48 -0300

    Atualização: utilizando somente @JsonIgnore

--- src/main/java/com/bergmannlucas/cursomc/cursomc/domain/Pagamento.java
+++ src/main/java/com/bergmannlucas/cursomc/cursomc/domain/Pagamento.java
@@ -1,7 +1,7 @@
 package com.bergmannlucas.cursomc.cursomc.domain;
 
 import com.bergmannlucas.cursomc.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,7 +17,7 @@ public abstract class Pagamento implements Serializable {
     private Integer id;
     private Integer estado;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "pedido_id")
     @MapsId
