225a18cbc2a132e5d58918d3a4063e692fce5bc6
bergmannlucas <lucaasberg@gmail.com>
2018-08-01 16:23:58 -0300

    Inserindo Pedido

--- src/main/java/com/bergmannlucas/cursomc/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/bergmannlucas/cursomc/cursomc/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.bergmannlucas.cursomc.cursomc.domain;
 
 import com.bergmannlucas.cursomc.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import javax.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
     private static final long serialVersionUID = 8456262643338925612L;
